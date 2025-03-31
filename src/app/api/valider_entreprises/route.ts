fb6274ce1a646fc7725487dbb43e32438274f245
GAMBLIN Vincent <yuumie31@gmail.com>
2025-03-31 10:16:16 +0200

    correction

--- src/app/api/valider_entreprises/route.ts
+++ src/app/api/valider_entreprises/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../lib/prisma";
+import { prisma } from "../../../lib/prisma";
 
 // Gestion de la requête GET (récupérer les entreprises non validées)
 export const GET = async (req: NextRequest) => {
